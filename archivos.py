715265b4ae7955c3248cfc0bba4a746131d1e8b2
alangamboa97 <alangam97@gmail.com>
2020-05-05 18:46:50 -0500

    Archivos

--- archivos.py
+++ archivos.py
@@ -1,1 +1,20 @@
 import turtle
+
+def main():
+    filename = input("Ingresa el nombre del archivo")
+
+    t = turtle.Turtle()
+
+    screen = t.getscreen()
+
+    archivo = open(filename, "r")
+
+    for linea in archivo:
+
+        text = linea.strip() #une todos las lineas
+
+        comandos = text.split(",") #lee las instrucciones separadas por un ,
+
+
+
+        comando = comandos[0]
